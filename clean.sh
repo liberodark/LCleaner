d695d59ac558666e9dd2c32f5a6fd32fbb3f20f1
liberodark <liberodark@gmail.com>
2018-08-31 12:26:17 +0200

    fix

--- clean.sh
+++ clean.sh
@@ -1,15 +1,15 @@
-package = /var/cache/pacman/pkg && /var/cache/apt/archives/ && /var/cache/yum/$basearch/$releasever/
-cache = ~/.cache/
-trash = ~/.local/share/Trash/files/
-journal = /var/log/
+$package = /var/cache/pacman/pkg && /var/cache/apt/archives/ && /var/cache/yum/$basearch/$releasever/
+$cache = ~/.cache/
+$trash = ~/.local/share/Trash/files/
+$journal = /var/log/
 
 
-# Check Cuberite
-ls /home/cuberite/ &> /dev/null
+# Check Journal
+ls /var/log/ &> /dev/null
 
 if [ "$?" != 0 ]; then
-    echo "Cuberite not Installed"
-    mkdir /home/cuberite/
+    echo "Journal is empty"
 else
-    echo "Cuberite is Installed"
+    echo "Journal is cleaned"
+    rm -r $journal
 fi
