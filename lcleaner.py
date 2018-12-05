48a347b338717166a2128fa5859f89ebdfa008d7
Florian <40731894+Minzord@users.noreply.github.com>
2018-12-05 17:51:47 +0100

    Update

--- lcleaner.py
+++ lcleaner.py
@@ -30,15 +30,23 @@ icon = PhotoImage(file="/usr/share/icons/lcleaner.png")  # link to the window ic
 windows.tk.call('wm', 'iconphoto', windows._w, icon)  # window application
 
 TEXTE = Label(windows, text='Linux Cleaner', fg="blue")
+TEXTE.config(font=("Arial", 20))
+
 TEXTE2 = Label(windows, text="LCleaner clean your linux distro.", fg="purple")
+TEXTE2.config(font=("Arial", 14))
+
 TEXTE.pack(side=TOP, padx=5, pady=3)  # Title of the text application
 TEXTE2.pack(side=TOP, padx=5, pady=10)  # Title of the text application
 
 CLEAN = Button(windows, text='CLEAN', height=2, width=30)
 RESTORE = Button(windows, text='RESTORE', height=2, width=30)
 
+CLEAN.pack()
+RESTORE.pack()
+
 INFO = Label(windows, text='', fg="black")  # Information that changes according to the button pressed by the user
 MESSAGE = Label(windows, text='STATE OF THE PC', fg="blue")
+MESSAGE.config(font=("Arial", 10))
 
 INFO.pack(side=BOTTOM)  # we close the buttons by deciding on their location
 MESSAGE.pack(side=BOTTOM)
